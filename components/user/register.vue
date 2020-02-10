b1a203fe78ed13a79d1c68c569d6a47a6f10fd45
change <xjx903697869@163.com>
2020-02-11 00:28:58 +0800

    12-表表单的验证

--- components/user/register.vue
+++ components/user/register.vue
@@ -1,11 +1,12 @@
 <template>
   <el-form :model="form" ref="form" :rules="rules" class="form">
-    <el-form-item class="form-item">
+    <!-- 添加对应的prop属性 -->
+    <el-form-item class="form-item" prop="username">
       <!-- 动态绑定表单 v-model -->
       <el-input placeholder="用户名手机" v-model="form.username"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="captcha">
       <el-input placeholder="验证码" v-model="form.captcha">
         <template slot="append">
           <el-button @click="handleSendCaptcha">发送验证码</el-button>
@@ -13,15 +14,15 @@
       </el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="nickname">
       <el-input placeholder="你的名字" v-model="form.nickname"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="password">
       <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
     </el-form-item>
 
-    <el-form-item class="form-item">
+    <el-form-item class="form-item" prop="checkPassword">
       <el-input placeholder="确认密码" type="password" v-model="form.checkPassword"></el-input>
     </el-form-item>
 
@@ -32,6 +33,18 @@
 <script>
 export default {
   data() {
+    //elment-ui  确认密码
+    //   rule--规则  value--值
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请再次输入密码"));
+      } else if (value !== this.form.password) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+
     return {
       // 表单数据
       form: {
@@ -42,7 +55,42 @@ export default {
         captcha: "" // 手机验证码
       },
       // 表单规则
-      rules: {}
+      rules: {
+        username: [
+          {
+            required: true,
+            message: "请输入用户名",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          }
+        ],
+        checkPassword: [
+          {
+            validator: validatePass,
+            trigger: "blur"
+          }
+        ],
+        nickname: [
+          {
+            required: true,
+            message: "请输入昵称",
+            trigger: "blur"
+          }
+        ],
+        captcha: [
+          {
+            required: true,
+            message: "请输入验证码",
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
