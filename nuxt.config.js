5ade709a1be9f3d3d45dcaec2b86a67d4ff7f95c
change <xjx903697869@163.com>
2020-02-11 01:26:49 +0800

    15-添加axios拦截手机格式错误信息

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,8 @@ export default {
    */
   plugins: [
     "@/plugins/element-ui",
-    { src: "@/plugins/localStorage", ssr: false }
+    { src: "@/plugins/localStorage", ssr: false },
+    "@/plugins/axios"
   ],
   /*
    ** Nuxt.js dev-modules
